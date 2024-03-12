3bdd39ca259b18997bdc9ede13d0cc8bbd736b3f
Joao de Freitas <49395421+jffvale@users.noreply.github.com>
2024-03-12 12:00:50 -0300

    Add files via upload

---
+++ delicious-master/js/active.js
@@ -0,0 +1,164 @@
+(function ($) {
+    'use strict';
+
+    var browserWindow = $(window);
+
+    // :: 1.0 Preloader Active Code
+    // browserWindow.on('load', function () {
+    //     $('#preloader').fadeOut('1', function () {
+    //         $(this).remove();
+    //     });
+    // });
+
+    // :: 2.0 Newsticker Active Code
+    $.simpleTicker($("#breakingNewsTicker"), {
+        speed: 1250,
+        delay: 3500,
+        easing: 'swing',
+        effectType: 'roll'
+    });
+
+    // :: 3.0 Nav Active Code
+    if ($.fn.classyNav) {
+        $('#deliciousNav').classyNav();
+    }
+
+    // :: 4.0 Search Active Code
+    var searchwrapper = $('.search-wrapper');
+    $('.search-btn').on('click', function () {
+        searchwrapper.toggleClass('on');
+    });
+    $('.close-btn').on('click', function () {
+        searchwrapper.removeClass('on');
+    });
+
+    // :: 5.0 Sliders Active Code
+    if ($.fn.owlCarousel) {
+        var welcomeSlide = $('.hero-slides');
+        var receipeSlide = $('.receipe-slider');
+
+        welcomeSlide.owlCarousel({
+            items: 1,
+            margin: 0,
+            loop: true,
+            nav: true,
+            navText: ['Prev', 'Next'],
+            dots: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            smartSpeed: 1000
+        });
+
+        welcomeSlide.on('translate.owl.carousel', function () {
+            var slideLayer = $("[data-animation]");
+            slideLayer.each(function () {
+                var anim_name = $(this).data('animation');
+                $(this).removeClass('animated ' + anim_name).css('opacity', '0');
+            });
+        });
+
+        welcomeSlide.on('translated.owl.carousel', function () {
+            var slideLayer = welcomeSlide.find('.owl-item.active').find("[data-animation]");
+            slideLayer.each(function () {
+                var anim_name = $(this).data('animation');
+                $(this).addClass('animated ' + anim_name).css('opacity', '1');
+            });
+        });
+
+        $("[data-delay]").each(function () {
+            var anim_del = $(this).data('delay');
+            $(this).css('animation-delay', anim_del);
+        });
+
+        $("[data-duration]").each(function () {
+            var anim_dur = $(this).data('duration');
+            $(this).css('animation-duration', anim_dur);
+        });
+
+        var dot = $('.hero-slides .owl-dot');
+        dot.each(function () {
+            var index = $(this).index() + 1 + '.';
+            if (index < 10) {
+                $(this).html('0').append(index);
+            } else {
+                $(this).html(index);
+            }
+        });
+
+        receipeSlide.owlCarousel({
+            items: 1,
+            margin: 0,
+            loop: true,
+            nav: true,
+            navText: ['Prev', 'Next'],
+            dots: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+            smartSpeed: 1000
+        });
+    }
+
+    // :: 6.0 Gallery Active Code
+    if ($.fn.magnificPopup) {
+        $('.videobtn').magnificPopup({
+            type: 'iframe'
+        });
+    }
+
+    // :: 7.0 ScrollUp Active Code
+    if ($.fn.scrollUp) {
+        browserWindow.scrollUp({
+            scrollSpeed: 1500,
+            scrollText: '<i class="fa fa-angle-up"></i>'
+        });
+    }
+
+    // :: 8.0 CouterUp Active Code
+    if ($.fn.counterUp) {
+        $('.counter').counterUp({
+            delay: 10,
+            time: 2000
+        });
+    }
+
+    // :: 9.0 nice Select Active Code
+    if ($.fn.niceSelect) {
+        $('select').niceSelect();
+    }
+
+    // :: 10.0 wow Active Code
+    if (browserWindow.width() > 767) {
+        new WOW().init();
+    }
+
+    // :: 11.0 prevent default a click
+    $('a[href="#"]').click(function ($) {
+        $.preventDefault()
+    });
+
+})(jQuery);
+
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    //Aqui vem o código
+
+    document.querySelector('#contact').addEventListener('click', () => {
+        let classe = document.getElementById('home');
+        classe.classList.remove('active');
+
+        classe = document.querySelector('#recipes');
+        classe.classList.remove('active');
+
+        classe = document.querySelector('#about');
+        classe.classList.remove('active');
+
+        classe = document.getElementById('contact');
+        classe.classList.remove('active');
+        classe.classList.add('active');
+    });
+
+    console.log("Somente serei executado quando o navegador terminar de analisar todo o DOM");
+
+});
